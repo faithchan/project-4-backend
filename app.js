fc6d9425a79cf5f4f23283e337e13da4666578bf
chloet <chloet@chloetklaytns-MacBook-Pro.local>
2022-01-29 10:51:53 +0800

    added usersModel

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
+
 const cors = require('cors')
+const CORS_WHITELIST = process.env.CORS_WHITELIST.split(',')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.use(
+  cors({
+    origin: CORS_WHITELIST,
+  })
+)
 
 app.get('/', async (req, res) => {
   res.send('hello world')
---
+++ models/usersModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  walletAddress: {
+    type: String,
+    unique: true,
+  },
+  tokensCreated: {
+    type: Number,
+    unique: true,
+  },
+  tokensOwned: {
+    type: Number,
+    unique: true,
+  },
+})
+
+userSchema.pre('save', function (next) {
+  let user = this
+  bcrypt.hash(user.password, SALT_WORK_FACTOR, (err, hash) => {
+    if (err) return next(err)
+    user.password = hash
+    next()
+  })
+})
+
+module.exports = mongoose.model('User', userSchema)
